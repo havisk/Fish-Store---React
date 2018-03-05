12d3f89cb8b6518ae34d683c19038ef6c215dccd
Kool Havis <havisk@gmail.com>
2018-03-05 13:10:42 -0500

    added premade css

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='catch-of-the-day'>
+        <p>hello</p>
+      </div>
+    )
+  }
+}
+
+
+export default App;
