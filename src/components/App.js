7bfeebf82bf37e7a7ae3c036ff50870a58a553f5
Kool Havis <havisk@gmail.com>
2018-03-08 22:41:19 -0500

    addFish component connected functionality

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,24 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    //copy of existing state
+    const fishes = {...this.state.fishes};
+
+    //add new fish
+    fishes[`fish${Date.now()}`] = fish;
+
+    //set the new fishes object to state
+    this.setState({fishes})
+  };
+
+
   render() {
     return (
       <div className='catch-of-the-day'>
@@ -14,7 +32,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
